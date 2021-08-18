723253ecb31661c32df2a53a9c2e23622f7480dd
bee-san <github@skerritt.blog>
2021-08-18 18:15:55 +0100

    Create Library & CLI
    You can now use it in library mode and CLI mode!
    
    The CLI should use the library and only parse the CLI, this means that this should be 100% library first with CLI as a second option.
    
    This allows us to do more things in the future like a nice API :)

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! Ares is an automatic decoding and cracking tool.
+
+pub fn crack() {
+    println!("Test");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
