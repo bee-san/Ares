66aa3da4653c66ce1bd504eac2e55f784a86b2a2
swanandx <73115739+swanandx@users.noreply.github.com>
2022-07-19 19:23:41 +0530

    feat: using include_dir! to load directory at compile time

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,10 +1,20 @@
-use std::collections::HashSet;
+use include_dir::Dir;
+use include_dir::include_dir;
 use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
-pub static STORAGE: Lazy<HashSet<&str>> = Lazy::new(|| {
-    let hashset_for_words: HashSet<&str> = include_str!("dictionaries/words.txt")
-        .split_ascii_whitespace()
-        .collect();
+pub static DICTIONARIES: Lazy<HashMap<&str, HashSet<&str>>> = Lazy::new(|| {
+    static DICTIONARIES_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/src/storage/dictionaries");
+    let mut entries = HashMap::new();
 
-    hashset_for_words
+    for entry in DICTIONARIES_DIR.files() {
+        let content = entry.contents_utf8().unwrap();
+        let hash_set: HashSet<&str> = content.split_ascii_whitespace().collect();
+
+        let filename = entry.path().to_str().unwrap();
+
+        entries.insert(filename, hash_set);
+    }
+    entries
 });
