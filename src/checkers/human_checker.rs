2dc67d16c36f1820f36a858f20493025564b6dcd
swanandx <73115739+swanandx@users.noreply.github.com>
2022-07-21 22:15:08 +0530

    feat: human chekers

--- src/checkers/human_checker.rs
+++ src/checkers/human_checker.rs
@@ -1,9 +1,11 @@
-use text_io::read;
-
 use crate::checkers::checker_object::CheckObject;
 
-#[allow(dead_code)]
-pub fn human_checker(input: CheckObject) -> bool {
+#[cfg(not(test))]
+use text_io::read;
+
+// compile this if we are not running tests
+#[cfg(not(test))]
+pub fn human_checker(input: &CheckObject) -> bool {
     let output_string = format!(
         "Is the plaintext '{}' which is {}. [Y/n]? ",
         input.text, input.description
@@ -14,5 +16,11 @@ pub fn human_checker(input: CheckObject) -> bool {
     if reply.starts_with('n') {
         return false;
     }
+    true
+}
+
+// use this human_checker for tests
+#[cfg(test)]
+pub fn human_checker(_input: &CheckObject) -> bool {
     true
 }
