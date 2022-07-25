2382baa0d5bca7395358d61632f33b1d724efc71
Bee <github@skerritt.blog>
2022-07-25 09:47:44 +0100

    Create benchmarking infrastructure for Ares (#31)
    * Create benchmarking infrastructure for Ares
    
    This PR creates a benchmarking infrastructure for our program with a
    github action that runs for pull requests. There is only 1 benchmark for
    now, but I will add more later.
    
    Co-authored-by: bee <bee@skerritt.blog>
    Co-authored-by: swanandx <73115739+swanandx@users.noreply.github.com>

--- src/crackers/mod.rs
+++
@@ -1,4 +0,0 @@
-//! This module contains the code for all of the crackers and enables us to run them
-//! A cracker is anything that cracks encrypted text
-//! Encrypted text is anything that is encrypted with a key.
-//! You can call each cracker directly, but it is best to use the searcher for this.
