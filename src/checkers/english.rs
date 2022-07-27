807424e6b917239a2b0e7e28c492053385f7c5d4
bee <bee@skerritt.blog>
2022-07-27 08:45:57 +0100

    Update English checker
    This updates the English checker to work on multiple words.
    
    There are some notable changes in this PR:
    * Using `Cargo-Nextest` instead of the regular Cargo test, which runs
      tests faster
    
    There are 2 bugs in the English checker:
    
    ```rust
     // TODO: I think the below function iterates through each dictionary in turn.
                // Which means it'll try English.txt, then rockyou.txt etc
                // This is inefficient and makes it harder to compute what dictionary the word came from.
                // We should probably just use a single dictionary and assign the filenames to the values in the dictionary.
                // Like {"hello": "English.txt"} etc.
                // If we're using muiltiple dictionaries we may also have duplicated words which is inefficient.
                if storage::DICTIONARIES.iter().find(|(_, words)| words.contains(word)).is_some() {
                    trace!("Found word {} in English", word);
                    words_found += 1.0;
                }
    ```
    
    And:
    
    ```rust
     // TODO: There is a bug here. We are comparing how many words found to how many characters there are in bytes.
                // This means the numbers don't exactly match up. There may be some cases where this will break.
                // We are also typecasting to f64 instead of usize, which costs CPU cycles.
                if words_found / input.len() as f64 > PLAINTEXT_DETECTION_PERCENTAGE {
                    debug!("Found {} words in {}", words_found, input);
                    debug!("Returning from English chekcer successfully with {}", input);
                    plaintext_found = true;
                    break;
                }
    ```
    
    I will fix these in a future PR. This PR is designed to just let
    Skeletal work on a Caesar cipher :)

--- src/checkers/english.rs
+++ src/checkers/english.rs
@@ -2,7 +2,7 @@
 use crate::checkers::checker_result::CheckResult;
 use crate::storage;
 use lemmeknow::Identify;
-// use log::{debug, info, trace}; unused imports
+use log::{debug, trace};
 
 use crate::checkers::checker_type::{Check, Checker};
 
@@ -25,14 +25,41 @@ impl Check for Checker<EnglishChecker> {
     }
 
     fn check(&self, input: &str) -> CheckResult {
+        trace!("Checking English for sentence {}", input);
+        // If 50% of the words are in the english list, then we consider it english.
+        // This is the threshold at which we consider it english.
+        // TODO: Do we want to put this into a config somewhere?
+        const PLAINTEXT_DETECTION_PERCENTAGE: f64 = 0.0;
+        let mut words_found: f64 = 0.0;
+
         let mut plaintext_found = false;
-        let mut filename = "";
-        if let Some(result) = storage::DICTIONARIES
-            .iter()
-            .find(|(_, words)| words.contains(input))
-        {
-            plaintext_found = true;
-            filename = result.0; // result.0 is the filename
+        // TODO: Change this when the below bugs are fixed.
+        let filename = "English.txt";
+
+        // loop through all the words in the input
+        for word in input.split_whitespace() {
+            // if the word is in the english list, then we consider it english
+            // TODO: I think the below function iterates through each dictionary in turn.
+            // Which means it'll try English.txt, then rockyou.txt etc
+            // This is inefficient and makes it harder to compute what dictionary the word came from.
+            // We should probably just use a single dictionary and assign the filenames to the values in the dictionary.
+            // Like {"hello": "English.txt"} etc.
+            // If we're using muiltiple dictionaries we may also have duplicated words which is inefficient.
+            if storage::DICTIONARIES.iter().find(|(_, words)| words.contains(word)).is_some() {
+                trace!("Found word {} in English", word);
+                words_found += 1.0;
+            }
+
+            trace!("Checking word {} with words_found {} and input length: {}", word, words_found, input.len());
+            // TODO: There is a bug here. We are comparing how many words found to how many characters there are in bytes.
+            // This means the numbers don't exactly match up. There may be some cases where this will break.
+            // We are also typecasting to f64 instead of usize, which costs CPU cycles.
+            if words_found / input.len() as f64 > PLAINTEXT_DETECTION_PERCENTAGE {
+                debug!("Found {} words in {}", words_found, input);
+                debug!("Returning from English chekcer successfully with {}", input);
+                plaintext_found = true;
+                break;
+            }
         }
 
         CheckResult {
@@ -68,7 +95,7 @@ mod tests {
     #[test]
     fn test_check_multiple_words() {
         let checker = Checker::<EnglishChecker>::new();
-        assert_eq!(checker.check("and woody").is_identified, false);
+        assert_eq!(checker.check("and woody").is_identified, true);
     }
 
     #[test]
@@ -80,5 +107,11 @@ mod tests {
                 .is_identified,
             false
         );
+    }
+
+    #[test]
+    fn test_check_multiple_words2() {
+        let checker = Checker::<EnglishChecker>::new();
+        assert!(checker.check("preinterview hello dog").is_identified);
     }
 }
