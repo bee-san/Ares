e8352bcc9683e9c2a17dc681fc750efd4779db34
swanandx <73115739+swanandx@users.noreply.github.com>
2022-07-28 13:29:35 +0530

    timer in bfs

--- src/searchers/bfs.rs
+++ src/searchers/bfs.rs
@@ -1,7 +1,15 @@
 use log::trace;
 use std::collections::HashSet;
+use std::time::Duration;
 
-use crate::{decoders::crack_results::CrackResult, filtration_system::MyResults};
+use crate::filtration_system::MyResults;
+use crossbeam::{
+    channel::{after, bounded},
+    select,
+};
+
+/// Maximum duration for which we try to decoded text
+const TIMEOUT_DURATION: Duration = Duration::from_secs(60);
 
 /// Breadth first search is our search algorithm
 /// https://en.wikipedia.org/wiki/Breadth-first_search
@@ -10,7 +18,12 @@ pub fn bfs(input: &str) -> Option<String> {
     // all strings to search through
     let mut current_strings = vec![input.to_string()];
 
-    let mut exit_result: Option<CrackResult> = None;
+    // sender and receiver for exit result.
+    // It has capacity of 1 as there will only be one successful result.
+    let (result_sender, result_receiver) = bounded(1);
+
+    // this will send after duration
+    let timeout = after(TIMEOUT_DURATION);
 
     // loop through all of the strings in the vec
     while !current_strings.is_empty() {
@@ -25,7 +38,9 @@ pub fn bfs(input: &str) -> Option<String> {
                 // if it's Break variant, we have cracked the text successfully
                 // so just stop processing further.
                 MyResults::Break(res) => {
-                    exit_result = Some(res);
+                    result_sender
+                        .send(res)
+                        .expect("expected no send error while sending exit result");
                     None // short-circuits the iterator
                 }
                 MyResults::Continue(results_vec) => {
@@ -41,15 +56,27 @@ pub fn bfs(input: &str) -> Option<String> {
         // if we find an element that matches our exit condition, return it!
         // technically this won't check if the initial string matches our exit condition
         // but this is a demo and i'll be lazy :P
-        if let Some(exit_res) = exit_result {
-            let exit_str = exit_res
-                .unencrypted_text
-                .expect("No unencrypted text even after checker succeed!");
-            trace!("Found exit string: {}", exit_str);
-            return Some(exit_str);
-        }
-
         current_strings = new_strings;
+
+        select! {
+            recv(result_receiver) -> exit_result => {
+                if let Ok(exit_res) = exit_result {
+                    let exit_str = exit_res
+                        .unencrypted_text
+                        .expect("No unencrypted text even after checker succeed!");
+                    trace!("Found exit string: {}", exit_str);
+                    return Some(exit_str)
+                }
+            },
+            recv(timeout) -> _ => {
+                trace!("Timed out after {}s", TIMEOUT_DURATION.as_secs());
+                // instead of returning None, we can change return type of bfs
+                // to Result<Option<String>, TimeOutError>
+                // and then we can return TimeOutError here ( it can be a String too)
+                return None
+            },
+            default => continue,
+        }
 
         trace!("Refreshed the vector, {:?}", current_strings);
     }
