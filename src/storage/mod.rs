80cb3fd191879cfaed31df33a74ccb26ee38a4d3
Bee <github@skerritt.blog>
2022-08-01 19:05:18 +0100

    Change to expect instead of unwrap
    Before this was causing some confusing errors:
    https://discordapp.com/channels/754001738184392704/902967642158141520/1003724518050762862

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -13,10 +13,10 @@ pub static DICTIONARIES: Lazy<HashMap<&str, HashSet<&str>>> = Lazy::new(|| {
     let mut entries = HashMap::new();
 
     for entry in DICTIONARIES_DIR.files() {
-        let content = entry.contents_utf8().unwrap();
+        let content = entry.contents_utf8().expect("The file you moved into the dictionaries folder is not UTF-8. The storage module only works on UTF-8 files.");
         let hash_set: HashSet<&str> = content.split_ascii_whitespace().collect();
 
-        let filename = entry.path().to_str().unwrap();
+        let filename = entry.path().to_str().expect("Cannot turn filename of the file you moved into the Dictionaries folder into a string");
 
         entries.insert(filename, hash_set);
     }
