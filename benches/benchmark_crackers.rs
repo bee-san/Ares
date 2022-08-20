dbdd021e2c6768dd982dc5e3d349f727b622a644
Bee <github@skerritt.blog>
2022-08-20 17:48:08 +0100

    Fixes cargo bench (#56)
    * Fixes cargo bench
    
    * cargo fmt
    
    Co-authored-by: bee <bee@skerritt.blog>

--- benches/benchmark_crackers.rs
+++ benches/benchmark_crackers.rs
@@ -1,11 +1,13 @@
 use ares::checkers::athena::Athena;
 use ares::checkers::checker_type::{Check, Checker};
 use ares::checkers::CheckerTypes;
+use ares::config::{set_global_config, Config};
 use ares::decoders::base64_decoder::Base64Decoder;
 use ares::decoders::interface::{Crack, Decoder};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    set_global_config(Config::default());
     let decode_base64 = Decoder::<Base64Decoder>::new();
     let athena_checker = Checker::<Athena>::new();
     let checker = CheckerTypes::CheckAthena(athena_checker);
