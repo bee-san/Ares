2eac06f294048b1593c83f88f4cb89516fb76dd7
swanandx <73115739+swanandx@users.noreply.github.com>
2022-11-13 14:03:23 +0530

    feat: dictionary with &str as key and value

--- src/decoders/dictionary_decoder.rs
+++ src/decoders/dictionary_decoder.rs
@@ -1,21 +1,21 @@
-use std::{collections::HashMap, error::Error};
+use std::collections::HashMap;
 
 ///! Given a dictionary such as morse code or caesar
 ///! a mapping of X -> Y, decode that text.
-
-
 pub fn dictionary_decode(input: &str, dictionary: &HashMap<&str, &str>) -> Option<String> {
-    let mut outputStr = Vec::new();
-    for x in input.chars() {
-        let x = x.to_string().to_uppercase().to_owned();
-        // TODO support uppercase
-        if !dictionary.contains_key::<str>(&x) {
-            return None;
-        }
-        outputStr.push(dictionary[&x]);
+    let mut output_str: String = String::with_capacity(input.len());
 
+    for x in input.chars() {
+        let x = x.to_uppercase().to_string();
+        // TODO support uppercase
+        let Some(val) = dictionary.get(&*x) else {
+            return None;
+        };
+
+        output_str.push_str(val);
     }
-    return Some(outputStr.join(" "));
+
+    Some(output_str)
 }
 
 #[cfg(test)]
@@ -26,17 +26,17 @@ mod tests {
     fn test_dictionary_decode() {
         let input = "....-";
         let mut dictionary = HashMap::new();
-        dictionary.insert('.', 'e');
-        dictionary.insert('-', 'k');
-        let output = dictionary_decode(&input, &dictionary);
+        dictionary.insert(".", "e");
+        dictionary.insert("-", "k");
+        let output = dictionary_decode(input, &dictionary);
         assert_eq!(output.unwrap(), "eeeek");
     }
 
     #[test]
     fn test_dictionary_decode_invalid() {
         let input = ".";
-        let mut dictionary = HashMap::new();
-        let output = dictionary_decode(&input, &dictionary);
+        let dictionary = HashMap::new();
+        let output = dictionary_decode(input, &dictionary);
         assert!(output.is_none());
     }
 }
