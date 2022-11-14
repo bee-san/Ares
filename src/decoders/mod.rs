632fed419e67e4c0ba6f4bfa7b5e6eb304726f86
SkeletalDemise <29117662+SkeletalDemise@users.noreply.github.com>
2022-11-14 01:30:11 -0800

    Add Base32
    Adds a Base32 decoder.

--- src/decoders/mod.rs
+++ src/decoders/mod.rs
@@ -5,6 +5,7 @@
 //! mod.rs file
 //! you will also need to make it a public module in this file.
 
+pub mod base32_decoder;
 /// The base64_decoder module decodes base64
 /// It is public as we use it in some tests.
 pub mod base64_decoder;
