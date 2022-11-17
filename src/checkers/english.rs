cd899c81c87c2e40d2b6ed498d0ede1d409747eb
autumn skerritt <github@skerritt.blog>
2022-11-17 13:32:48 +0000

    Added function documentation

--- src/checkers/english.rs
+++ src/checkers/english.rs
@@ -83,8 +83,11 @@ impl Check for Checker<EnglishChecker> {
     }
 }
 
-// Strings look funny, they might have commas, be uppercase etc
-// This normalises the string so English checker can work on it
+///! Strings look funny, they might have commas, be uppercase etc
+///! This normalises the string so English checker can work on it
+///! In particular it:
+///! Removes puncuation from the string
+///! Lowercases the string
 fn normalise_string(input: &str) -> String {
     // The replace function supports patterns https://doc.rust-lang.org/std/str/pattern/trait.Pattern.html#impl-Pattern%3C%27a%3E-3
     // TODO add more puncuation
