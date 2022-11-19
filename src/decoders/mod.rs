49c5e75be2e8b09dbc2f5243c134a04aa2dfd8a3
SkeletalDemise <29117662+SkeletalDemise@users.noreply.github.com>
2022-11-19 00:23:18 -0800

    Add Base64 URL
    Adds a Base64 URL decoder.

--- src/decoders/mod.rs
+++ src/decoders/mod.rs
@@ -23,6 +23,8 @@ pub mod base58_flickr_decoder;
 /// The base64_decoder module decodes base64
 /// It is public as we use it in some tests.
 pub mod base64_decoder;
+/// The base64_url_decoder module decodes base64 url
+pub mod base64_url_decoder;
 /// The citrix_ctx1_decoder module decodes citrix ctx1
 pub mod citrix_ctx1_decoder;
 /// The crack_results module defines the CrackResult
