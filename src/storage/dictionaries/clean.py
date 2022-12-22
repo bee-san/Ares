866992c6ba01ae86d6c04c7a376c9afd69db4816
autumn skerritt <github@skerritt.blog>
2022-12-22 09:17:38 +0000

    Added comments

--- src/storage/dictionaries/clean.py
+++ src/storage/dictionaries/clean.py
@@ -1,4 +1,9 @@
+# Run this to clean up any dictinaries
+
+# We remove puncuation from the text in the english checker
+# therefore we should not have any in our dictionary
 PUNC = set("!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+# Anything below length of 2 is rather spammy so we remove it
 MIN_LENGTH = 2
 
 file_name = input("Enter the name of the file you want to modify: ")
