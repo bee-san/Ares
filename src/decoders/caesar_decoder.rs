b9572a3f766915545523d658f27374473c00b7a3
autumn skerritt <github@skerritt.blog>
2023-02-05 18:51:47 +0000

    change to assert true

--- src/decoders/caesar_decoder.rs
+++ src/decoders/caesar_decoder.rs
@@ -192,6 +192,7 @@ mod tests {
             // If we get here, the test passed
             // Because the caesar_decoder.crack function returned None
             // as it should do for the input
+            assert!(true)
         }
     }
 }
