72c4d6480cf96b339163bb4b3d756a43ede4d984
Gregor Niehl <gregorniehl@web.de>
2024-01-04 14:02:45 +0100

    main.rs: Remove a newline

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     // Turn CLI arguments into a library object
     let (text, config) = parse_cli_args();
     let result = perform_cracking(&text, config);
-
     match result {
         // TODO: As result have array of CrackResult used,
         // we can print in better way with more info
