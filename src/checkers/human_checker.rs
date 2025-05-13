c9db1586d62a19116dd16afc79dd8edea99bb894
bee <github@skerritt.blog>
2025-05-13 09:21:14 +0900

    fixed up braille and human checker

--- src/checkers/human_checker.rs
+++ src/checkers/human_checker.rs
@@ -4,9 +4,13 @@ use crate::config::get_config;
 use crate::{cli_pretty_printing, timer};
 use dashmap::DashSet;
 use std::sync::OnceLock;
+use std::sync::atomic::{AtomicBool, Ordering};
 use text_io::read;
 
 static SEEN_PROMPTS: OnceLock<DashSet<String>> = OnceLock::new();
+// if human checker is called, we set this to true
+// so we dont call it again
+static HUMAN_CONFIRMED: AtomicBool = AtomicBool::new(false);
 
 fn get_seen_prompts() -> &'static DashSet<String> {
     SEEN_PROMPTS.get_or_init(DashSet::new)
@@ -18,11 +22,11 @@ fn get_seen_prompts() -> &'static DashSet<String> {
 /// TODO: Add a way to specify a list of checkers to use in the library. This checker is not library friendly!
 // compile this if we are not running tests
 pub fn human_checker(input: &CheckResult) -> bool {
+    // Check if a human has already confirmed a result
+    if HUMAN_CONFIRMED.load(Ordering::Relaxed) {
+            return true;
+        }
     timer::pause();
-    println!(
-        "DEBUG: Human checker called from:\n{:?}",
-        std::backtrace::Backtrace::capture()
-    );
     // wait instead of get so it waits for config being set
     let config = get_config();
     // We still call human checker, just if config is false we return True
@@ -41,6 +45,11 @@ pub fn human_checker(input: &CheckResult) -> bool {
     let reply: String = read!("{}\n");
     cli_pretty_printing::success(&format!("DEBUG: Human checker received reply: '{}'", reply));
     let result = reply.to_ascii_lowercase().starts_with('y');
+    // If the user confirmed, set the atomic boolean to true
+    if result {
+        HUMAN_CONFIRMED.store(true, Ordering::Relaxed);
+        cli_pretty_printing::success("DEBUG: Human confirmed a result, future checks will be skipped");
+    }
     timer::resume();
 
     cli_pretty_printing::success(&format!("DEBUG: Human checker returning: {}", result));
